3b877ac7e6340dcc5b88404bd67399eeb3daf3f9
DanielRamsauer <danramsauer@gmail.com>
2022-12-30 17:36:06 -0500

    added notes and changed some styling

--- random-hex-generator/script.js
+++ random-hex-generator/script.js
@@ -10,11 +10,14 @@ const generateColor = () => {
     // substring tells us where to start so the 2 is after the 0. and the 8 is telling us where to end extracting the numbers / letters
     const randomColor = Math.random().toString(16).substring(2, 8);
 
+    // Changing background color to our randomly generated hex
     document.body.style.backgroundColor = `#${randomColor}`;
 
+    // changing the inner html of our box on the screen for the user to see
     hex.innerHTML = `#${randomColor}`;
 }
 
+// adding an event listener to generate a new color on click of the button
 button.addEventListener('click', generateColor);
 
 generateColor();

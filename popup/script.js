fff5740a63b607cdd89d59170b8f1d005de46f48
DanielRamsauer <danramsauer@gmail.com>
2023-01-03 14:01:37 -0500

    complated popup tester

--- popup/script.js
+++ popup/script.js
@@ -1,3 +1,22 @@
 // when clicked on text popup button have the popup move into display
-// document.getElement for the button 
+// document.querySelector for the button 
 // add event listener on click and have the modal-content move into site
+
+const button = document.querySelector('#button');
+const modal = document.querySelector('.modal');
+const close = document.querySelector('.close');
+
+button.addEventListener('click', openModal);
+close.addEventListener('click', closeModal);
+modal.addEventListener('click', closeModal);
+
+// open modal function
+function openModal () {
+    modal.style.display = 'block';
+    console.log('hi');
+}
+
+// close modal function
+function closeModal () {
+    modal.style.display = 'none';
+}

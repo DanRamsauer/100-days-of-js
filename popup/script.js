4391f4ae1b5502dd2fceacdac1da1ab12efbb7c0
DanielRamsauer <danramsauer@gmail.com>
2023-01-03 13:45:40 -0500

    finished css and html

---
+++ popup/script.js
@@ -0,0 +1,3 @@
+// when clicked on text popup button have the popup move into display
+// document.getElement for the button 
+// add event listener on click and have the modal-content move into site
